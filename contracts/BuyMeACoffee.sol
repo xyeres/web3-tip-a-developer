a5028faeb330ed087eaac5e59cfe6adfe58ddb6d
Michael Carr <xyeres@me.com>
2022-06-29 10:00:19 -0700

    add buyMeACoffee func

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,48 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract BuyMeACoffee {
+    // Emit when Memo is created
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+    // List of all memos received from friends
+    Memo[] memos;
+
+    // address of contract deployer
+    address payable owner;
+
+    // Deploy logic
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message a nice message from the coffee buyer
+     */
+    function buyCoffee(string memory _name, string memory _message) public payable { 
+        require(msg.value > 0, "Can't buy coffee with 0 eth");
+
+        // add memo to storage
+        memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        ));
+        // Emit a log event when a new memo is created
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+}
