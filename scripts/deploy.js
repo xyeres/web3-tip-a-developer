d7c4f6b0343c16243f59c78b91d8bf78784c551d
Michael Carr <xyeres@me.com>
2022-06-30 14:06:21 -0700

    add deployed address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,13 @@
 const hre = require('hardhat')
 
+// Deployed: 6/30/22 0xb8eFC509968689f06568d606F927003CDA4cc811
+
 async function main() {
   // get the contract and deploy 
   const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
   const buyMeACoffee = await BuyMeACoffee.deploy();
   await buyMeACoffee.deployed()
   console.log(`BuyMeACoffee deployed to `, buyMeACoffee.address)
-
 }
 
 main()
