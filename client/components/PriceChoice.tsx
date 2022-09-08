76a4f247e3ea919d67f467fc6af6599723dfabf7
Michael Carr <xyeres@me.com>
2022-09-07 18:18:54 -0700

    Add scale to transition

--- client/components/PriceChoice.tsx
+++ client/components/PriceChoice.tsx
@@ -11,15 +11,13 @@ type Props = {
 const PriceChoice = (props: Props) => {
   return (
     <div
-      className={`cursor-pointer relative w-[110px]  border-white hover:border-purple-400 transition-all flex flex-col items-center delay-300 justify-center gap-1  ${
-        props.isWallet ? "h-[110px]" : "h-[40px] overflow-hidden"
-      }`}
+      className={`cursor-pointer relative w-[110px] h-full border border-white hover:border-purple-400 transition-colors flex flex-col items-center justify-center gap-1}`}
     >
       <div className="flex flex-row items-center justify-center gap-1">
         <Image src={matic} width={32} height={32} alt="$MATIC Polygon" />
         <span className="text-[1.5rem] font-bold">{props.amount}</span>
       </div>
-      <span className="justify-self-stretch text-gray-400">
+      <span className="justify-self-stretchtext-gray-400">
         ABOUT {props.conversion}
       </span>
     </div>
--- client/pages/price.tsx
+++ client/pages/price.tsx
@@ -2,11 +2,11 @@ import { NextPage } from "next";
 import PriceChoice from "../components/PriceChoice";
 
 const Price: NextPage = () => {
-  const options = [
-    { amount: "3", conversion: "$2.4" },
-    { amount: "5", conversion: "$4" },
-    { amount: "10", conversion: "$8" },
-  ];
+const options = [
+  { amount: "3", conversion: "$2.4" },
+  { amount: "5", conversion: "$4" },
+  { amount: "10", conversion: "$8" },
+];
   return (
     <div className="flex flex-row flex-wrap items-center justify-center gap-4">
       {options.map((option) => (
