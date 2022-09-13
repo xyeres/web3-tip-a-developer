859948e56def95545ff8df0d4f4057b0ee989331
Michael Carr <xyeres@me.com>
2022-09-13 12:31:35 -0700

    add hover state

--- client/components/Memos.tsx
+++ client/components/Memos.tsx
@@ -14,7 +14,7 @@ const Memos = (props: Props) => {
       {props.memos.map((memo: Memo, i:number) => (
         <div
           key={i}
-          className="opacity-80 w-full rounded-xl border border-neutral-300 mt-[43px] p-6 flex flex-col"
+          className="hover:opacity-100 transition-opacity duration-200 opacity-80 w-full rounded-xl border border-neutral-300 mt-[43px] p-6 flex flex-col"
         >
           <p className="text-xl font-extralight">{memo.msg}</p>
           <span className="self-end mt-3 text-[.675rem]">
