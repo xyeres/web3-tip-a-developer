b50a0c90a19430d4aed62d26c2bb5a1f8b7cba3f
Michael Carr <xyeres@me.com>
2022-09-16 12:31:41 -0700

    add owner transfer test case

--- scripts/test-tip-contract.js
+++ scripts/test-tip-contract.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
 
-// Returns the Ether balance of a given address 
+// Returns the Ether balance of a given address
 async function getBalance(address) {
-  const balanceBigInt = await hre.waffle.provider.getBalance(address)
-  return hre.ethers.utils.formatEther(balanceBigInt)
+  const balanceBigInt = await hre.waffle.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
 }
 
 // Logs the Ether balance for a list of address
@@ -18,47 +18,78 @@ async function printBalances(addresses) {
 // Logs memos stored on-chain from tips
 async function printMemos(memos) {
   for (const memo of memos) {
-    const timestamp = memo.timestamp
-    const tipper = memo.name
-    const tipperAddress = memo.from
-    const message = memo.message
-    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`)
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(
+      `At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`
+    );
   }
 }
 
 async function main() {
   // get example accounts
-  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+  const [owner, newOwner, tipper2, tipper3] = await hre.ethers.getSigners();
 
-  // get the contract and deploy 
+  // get the contract and deploy
   const TipADeveloper = await hre.ethers.getContractFactory("TipADeveloper");
   const tipADeveloper = await TipADeveloper.deploy();
-  await tipADeveloper.deployed()
-  console.log(`TipADeveloper deployed to `, tipADeveloper.address)
+  await tipADeveloper.deployed();
+  console.log(`TipADeveloper deployed to `, tipADeveloper.address);
 
   // check balances before sending tip
-  const addresses = [owner.address, tipper3.address, tipper2.address, tipADeveloper.address]
-  console.log('==== start ===')
-  await printBalances(addresses)
+  const addresses = [
+    owner.address,
+    newOwner.address,
+    tipper3.address,
+    tipper2.address,
+    tipADeveloper.address,
+  ];
+  console.log("==== start ===");
+  await printBalances(addresses);
 
   // tip the contract owner a few coins
-  const tip = { value: hre.ethers.utils.parseEther('2') }
-  await tipADeveloper.connect(tipper3).tip("Bobby", "Love it", tip)
-  await tipADeveloper.connect(tipper2).tip("Tallbutt", "Cool!", tip)
-  console.log('==== tip sent ===')
-  await printBalances(addresses)
+  const tip = { value: hre.ethers.utils.parseEther("2") };
+  await tipADeveloper.connect(tipper3).tip("Bobby", "Love it", tip);
+  await tipADeveloper.connect(tipper2).tip("Tallbutt", "Cool!", tip);
+  console.log("==== tip sent ===");
+  await printBalances(addresses);
 
   // withdraw funds
-  // await tipADeveloper.connect(owner).withdrawTips();
+  console.log("=== withdraw tips ===");
+  await tipADeveloper.connect(newOwner).withdrawTips();
+  await printBalances(addresses);
 
-  // check balance after withdraw
-  console.log('==== did not withdraw tips ===')
-  await printBalances(addresses)
+  // Change Owner
+  console.log("=== transfering ownership of contract ===");
+  console.log("attempting to transfer...");
+  await tipADeveloper.connect(owner).transferOwner(newOwner.address);
+  console.log("transfer complete...");
+
+  // Make more tips from same people
+  const moreTip = { value: hre.ethers.utils.parseEther("10") };
+  await tipADeveloper
+    .connect(tipper3)
+    .tip("Bobby", "Love it again even with the new ownership", moreTip);
+  await tipADeveloper
+    .connect(tipper2)
+    .tip("Tallbutt", "Cool I like the new owner!", moreTip);
+  console.log("*** 2 tips for 10eth each are made... ***");
+
+  // Balance check
+  console.log("=== balances after ownership transfer and those 2 tips above ===");
+  await printBalances(addresses);
+
+  // withdraw funds to new owner
+  console.log("=== withdraw tips to new owner ===");
+  await tipADeveloper.connect(newOwner).withdrawTips();
+  await printBalances(addresses);
 
   //read all the memos left for the owner
-  console.log('==== memos ===')
-  const memos = await tipADeveloper.getMemos()
-  printMemos(memos)
+  console.log("==== memos ===");
+  const memos = await tipADeveloper.getMemos();
+  printMemos(memos);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
