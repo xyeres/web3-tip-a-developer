6941bdf0d3ce4c3c9626e59d450ef000636003ff
Michael Carr <xyeres@me.com>
2022-09-20 14:14:24 -0700

    add: timestamp

--- client/components/Memos.tsx
+++ client/components/Memos.tsx
@@ -1,7 +1,6 @@
 export type Memo = {
   address: string;
   timestamp: number;
-  date?: Date;
   name: string;
   message: string;
 };
@@ -15,12 +14,12 @@ const Memos = (props: Props) => {
     <>
       {props.memos.map((memo: Memo, i: number) => (
         <div
-          key={i}
-          className="hover:opacity-100 transition-opacity duration-200 opacity-80 w-full rounded-xl border border-neutral-300 mt-[43px] p-6 flex flex-col"
+          key={memo.timestamp.toString()}
+          className="hover:opacity-100 transition-opacity duration-200 opacity-80 w-full border border-neutral-700 mt-[43px] p-6 flex flex-col"
         >
           <p className="text-xl font-extralight">{memo.message}</p>
           <span className="self-end mt-3 text-[.675rem]">
-            &mdash; {memo.name}
+            <>&mdash; {memo.name} on {(new Date(memo.timestamp * 1000)).toLocaleDateString()}</>
           </span>
         </div>
       ))}
