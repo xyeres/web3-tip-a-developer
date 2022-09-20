841a120551d90350a6f14b5defecf1d865f93c71
Michael Carr <xyeres@me.com>
2022-09-19 18:25:30 -0700

    update interface

--- client/components/Memos.tsx
+++ client/components/Memos.tsx
@@ -1,7 +1,9 @@
 export type Memo = {
-  msg: string;
-  author: string;
-  amount: string;
+  address: string;
+  timestamp: number;
+  date?: Date;
+  name: string;
+  message: string;
 };
 
 type Props = {
@@ -11,14 +13,14 @@ type Props = {
 const Memos = (props: Props) => {
   return (
     <>
-      {props.memos.map((memo: Memo, i:number) => (
+      {props.memos.map((memo: Memo, i: number) => (
         <div
           key={i}
           className="hover:opacity-100 transition-opacity duration-200 opacity-80 w-full rounded-xl border border-neutral-300 mt-[43px] p-6 flex flex-col"
         >
-          <p className="text-xl font-extralight">{memo.msg}</p>
+          <p className="text-xl font-extralight">{memo.message}</p>
           <span className="self-end mt-3 text-[.675rem]">
-            &mdash; {memo.author} with {memo.amount}
+            &mdash; {memo.name}
           </span>
         </div>
       ))}
