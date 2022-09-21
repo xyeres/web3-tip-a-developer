758c476784a01d817edf7c984a1ee0a7d9410cb1
Michael Carr <xyeres@me.com>
2022-09-21 12:30:50 -0700

    add: marquee slider

--- client/components/Memos.tsx
+++ client/components/Memos.tsx
@@ -1,8 +1,12 @@
+import Marquee from "react-marquee-slider";
+import styled from "styled-components";
+
 export type Memo = {
   address: string;
   timestamp: number;
   name: string;
   message: string;
+  date: string;
 };
 
 type Props = {
@@ -11,19 +15,31 @@ type Props = {
 
 const Memos = (props: Props) => {
   return (
-    <>
-      {props.memos.map((memo: Memo, i: number) => (
-        <div
-          key={memo.timestamp.toString()}
-          className="hover:opacity-100 hover:bg-neutral-800 bg-[#121212] transition-all duration-150 opacity-80 w-full mt-[43px] p-6 flex flex-col"
-        >
-          <p className="text-xl font-extralight">{memo.message}</p>
-          <span className="self-end mt-3 text-[.675rem]">
-            <>&mdash; {memo.name} on {(new Date(memo.timestamp * 1000)).toLocaleDateString()}</>
-          </span>
-        </div>
-      ))}
-    </>
+    <div>
+      <Marquee
+        velocity={25}
+        scatterRandomly={false}
+        onFinish={() => { }}
+        onInit={() => { }}
+        direction="rtl"
+        resetAfterTries={200}
+      >
+        {props.memos.map((memo: Memo) => (
+          <div
+            tabIndex={0}
+            key={memo.timestamp.toString()}
+            className="hover:opacity-100 focus:ring-4 outline-none ring-neutral-700 hover:bg-neutral-800 bg-[#121212] transition-all duration-150 opacity-80 mt-[43px] mb-4 ml-10 p-6 flex flex-col"
+          >
+            <p className="text-xl font-extralight">{memo.message}</p>
+            <span className="self-end mt-3 text-[.675rem]">
+              <>
+                &mdash; {memo.name} on {memo.date}
+              </>
+            </span>
+          </div>
+        ))}
+      </Marquee>
+    </div>
   );
 };
 
