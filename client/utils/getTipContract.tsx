a1c9351f4a6d8e10eac8bb306583dd939bf85a10
Michael Carr <xyeres@me.com>
2022-09-28 15:07:30 -0700

    clean: move file

---
+++ client/utils/getTipContract.tsx
@@ -0,0 +1,19 @@
+import { Contract, ethers } from "ethers";
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from "../lib/constants";
+
+/**
+ * Return a contract signed by a ethers VoidSigner
+ * @returns Contract
+ */
+export function getTipContract(): Contract {
+  let defaultProvider = ethers.providers.getDefaultProvider(
+    process.env.NEXT_PUBLIC_ALCHEMY_POLYGON_TESTNET_RPC,
+    {
+      alchemy: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+    }
+  );
+  
+  let signer = new ethers.VoidSigner(CONTRACT_ADDRESS, defaultProvider);
+
+  return new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+}
