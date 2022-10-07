1f420f1ec166d9bd0be0957ab3d3d8d0028093d3
Michael Carr <xyeres@me.com>
2022-10-07 11:53:33 -0700

    fix: hover animation

--- client/components/Pill.tsx
+++ client/components/Pill.tsx
@@ -12,8 +12,8 @@ type Props = {
 function Pill({ icon, title, platform, link }: Props): JSX.Element {
   return (
     <Link href={link}>
-      <a className="flex focus:ring-4 outline-none my-2 py-[5px] p-2 pr-4 bg-gray-600 focus:bg-gray-700 hover:bg-gray-700 rounded-3xl ring-gray-400">
-        <span className="text-white transition-colors duration-200  text-xs inline-flex flex-row gap-2 items-center">
+      <a className="flex focus:ring-4 transition-colors duration-300 outline-none my-2 py-[5px] p-2 pr-4 bg-gray-600 focus:bg-gray-700 hover:bg-gray-700 rounded-3xl ring-gray-400">
+        <span className="text-white  text-xs inline-flex flex-row gap-2 items-center">
           <span className="flex-shrink-0 flex items-center">
             <Image alt={platform} src={icon} />
           </span>
