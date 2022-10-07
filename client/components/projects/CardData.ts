0591cb37eac20f6f8db27e4204a6a844963715ab
Michael Carr <xyeres@me.com>
2022-10-06 20:56:55 -0700

    feat: add href prop

--- client/components/projects/CardData.ts
+++ client/components/projects/CardData.ts
@@ -10,8 +10,9 @@ export type Card = {
   logoOnly: boolean;
   desc: string;
   role: string;
-  stack: string;
+  stack: Array<String>;
   published: boolean;
+  href: string;
 };
 
 const cardData: Card[] = [
@@ -20,23 +21,24 @@ const cardData: Card[] = [
     imgWidth: 40,
     imgHeight: 40,
     classNames: {
-      stack: "bg-pink-300",
-      container: "bg-[#fef7ff] ring-pink-100 border-pink-200",
+      stack: "bg-fuchsia-100",
+      container: "bg-[#fef7ff] ring-fuchsia-100 border-fuchsia-200",
     },
     title: "Constellation Labs",
     logoOnly: false,
     desc: `Anyone can fund their next project through decentralized web3
     crowdfunding. Coming soon.`,
     role: "UI Design, Development, Architecture",
-    stack: "Solidity, ethers.js, Next.js",
+    stack: ["Solidity", "ethers.js", "Next.js"],
     published: false,
+    href: "#",
   },
   {
     imgSrc: "/imgs/ss-logo-full.svg",
     imgWidth: 200,
     imgHeight: 46,
     classNames: {
-      stack: "bg-blue-300",
+      stack: "bg-blue-100",
       container:
         "bg-gradient-to-b from-[#fbffff] border-blue-200 ring-blue-100",
     },
@@ -45,15 +47,16 @@ const cardData: Card[] = [
     desc: `SaaS processing 10M+ monthly rows of royalty data for labels
     and publishers`,
     role: "UI Design, Development, System Design",
-    stack: "React, Django, PostgreSQL",
+    stack: ["React", "Django", "PostgreSQL"],
     published: true,
+    href: "https://www.soundsplits.com",
   },
   {
     imgSrc: "",
     imgWidth: 40,
     imgHeight: 40,
     classNames: {
-      stack: "bg-yellow-200",
+      stack: "bg-yellow-100",
       container:
         "bg-gradient-to-b from-yellow-50 border-yellow-300 ring-yellow-200",
     },
@@ -62,8 +65,9 @@ const cardData: Card[] = [
     desc: `Subscribers-only music streaming for United Pursuit Records
     delivering exclusive music to their most loyal fans`,
     role: "Design & Development",
-    stack: "Next.js, React, Firebase",
+    stack: ["Next.js", "React", "Firebase"],
     published: true,
+    href: "https://listen.unitedpursuit.com",
   },
 ];
 
